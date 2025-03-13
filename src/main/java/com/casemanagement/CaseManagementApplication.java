bc8630f38ab05bc9022c307b1863c768a3a11afc
Roland Dunee <rdunee@gmail.com>
2025-03-13 20:19:50 +0200

    initialization

---
+++ src/main/java/com/casemanagement/CaseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.casemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casemanagement/CaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
